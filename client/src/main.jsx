132661871d1c62518c8dfc3f9b3130706bca229c
turbolat <lawalat@turbolat-tech.com>
2026-03-03 17:18:50 +0100

    add redux toolkit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { store } from './redux/store.js'
+import { Provider} from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <Provider store={store}>
+    <App/>
+  </Provider>
+);
+
+
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+// import { store } from './redux/store.js'
+// import { Provider} from 'react-redux'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <Provider store={store}>
+//     <App/>
+//   </Provider>
+// );
+
